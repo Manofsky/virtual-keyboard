1f95655cf3dff090d2846fac3a9769b8122f3705
Manofsky <clegrof@gmail.com>
2022-05-08 22:27:08 +0300

    feat: implement the animation of pressing keys on the virtual keyboard

--- script.js
+++ script.js
@@ -30,12 +30,28 @@ function getKeyboardWrappContent() {
 }
 keyboardWrapp.append(getKeyboardWrappContent());
 
+function pressVirtualKeyboard() {
+  let eventTarget;
+  document.addEventListener('mousedown', function (event) {
+    eventTarget = event.target;
+    if (eventTarget.innerHTML === 'Caps Lock') { event.target.classList.toggle('pressed'); }
+    else if (eventTarget.innerHTML.length < 10 && eventTarget.innerHTML.length > 0) { event.target.classList.add('pressed'); }
+  });
+  document.addEventListener('mouseup', function () {
+    if (eventTarget.innerHTML !== 'Caps Lock') {
+      eventTarget.classList.remove('pressed');
+    }
+  });
+}
+pressVirtualKeyboard();
+
 let row1 = document.querySelector("body > div > div > div:nth-child(1)");
 let row2 = document.querySelector("body > div > div > div:nth-child(2)");
 let row3 = document.querySelector("body > div > div > div:nth-child(3)");
 let row4 = document.querySelector("body > div > div > div:nth-child(4)");
 let row5 = document.querySelector("body > div > div > div:nth-child(5)");
 
+let arrRow1Keycode = [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8];
 let arrRow1LowerCaseEn = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
 let arrRow1CapsLockEn = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
 let arrRow1ShiftEn = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'];
@@ -44,23 +60,28 @@ let arrRow1CapsLockRu = ['Ё'];
 let arrRow1ShiftRu = ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace'];
 
 
-
+let arrRow2Keycode = [9, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 220, 46];
 let arrRow2LowerCaseEn = ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del'];
 let arrRow2ShiftEn = ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'Del'];
 let arrRow2CapsLockEn = ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'Del'];
 
+let arrRow3Keycode = [20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 13];
 let arrRow3LowerCaseEn = ['Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'];
 let arrRow3ShiftEn = ['Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter'];
 
+let arrRow4Keycode = [16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 38, 16];
 let arrRow4LowerCaseEn = ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'Shift'];
 let arrRow4ShiftEn = ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '▲', 'Shift'];
 
-let arrRow5LowerCaseEn = ['Ctrl', 'Win', 'Alt', '', 'Alt', '◄', '▼', '►', 'Ctrl'];
+let arrRow5Keycode = [17, 91, 18, 32, 18, 37, 40, 39, 17];
+let arrRow5LowerCaseEn = ['Ctrl', 'Win', 'Alt', ' ', 'Alt', '◄', '▼', '►', 'Ctrl'];
+let arrRow5ShiftEn = [];
 
 class RowContent {
-  constructor(arrLowerCaseEn, arrShiftEn) {
+  constructor(arrLowerCaseEn, arrShiftEn, arrRowKeycode) {
     this.arrLowerCaseEn = arrLowerCaseEn;
     this.arrShiftEn = arrShiftEn;
+    this.arrRowKeycode = arrRowKeycode;
   }
 
   getRowContent() {
@@ -74,11 +95,11 @@ class RowContent {
     return fragment;
   }
 }
-let row1LowerCaseEn = new RowContent(arrRow1LowerCaseEn);
-let row2LowerCaseEn = new RowContent(arrRow2LowerCaseEn);
-let row3LowerCaseEn = new RowContent(arrRow3LowerCaseEn);
-let row4LowerCaseEn = new RowContent(arrRow4LowerCaseEn);
-let row5LowerCaseEn = new RowContent(arrRow5LowerCaseEn);
+let row1LowerCaseEn = new RowContent(arrRow1LowerCaseEn, arrRow1ShiftEn, arrRow1Keycode);
+let row2LowerCaseEn = new RowContent(arrRow2LowerCaseEn, arrRow2ShiftEn, arrRow2Keycode);
+let row3LowerCaseEn = new RowContent(arrRow3LowerCaseEn, arrRow3ShiftEn, arrRow3Keycode);
+let row4LowerCaseEn = new RowContent(arrRow4LowerCaseEn, arrRow4ShiftEn, arrRow4Keycode);
+let row5LowerCaseEn = new RowContent(arrRow5LowerCaseEn, arrRow5ShiftEn, arrRow5Keycode);
 row1.append(row1LowerCaseEn.getRowContent());
 row2.append(row2LowerCaseEn.getRowContent());
 row3.append(row3LowerCaseEn.getRowContent());
@@ -106,5 +127,130 @@ shiftLeft.className = "keys shift-left-key";
 let shiftRight = document.querySelector("body > div > div > div:nth-child(4) > div:nth-child(13)");
 shiftRight.className = "keys shift-right-key";
 
+let controlLeft = document.querySelector("body > div > div > div:nth-child(5) > div:nth-child(1)");
+let controlRight = document.querySelector("body > div > div > div:nth-child(5) > div:nth-child(9)");
+
+let altLeft = document.querySelector("body > div > div > div:nth-child(5) > div:nth-child(3)");
+let altRight = document.querySelector("body > div > div > div:nth-child(5) > div:nth-child(5)");
+
+let meta = document.querySelector("body > div > div > div:nth-child(5) > div:nth-child(2)");
+let arrowLeft = document.querySelector("body > div > div > div:nth-child(5) > div:nth-child(6)");
+let arrowUp = document.querySelector("body > div > div > div:nth-child(4) > div:nth-child(12)");
+let arrowDown = document.querySelector("body > div > div > div:nth-child(5) > div:nth-child(7)");
+let arrowRight = document.querySelector("body > div > div > div:nth-child(5) > div:nth-child(8)");
+
 let whitespace = document.querySelector("body > div > div > div:nth-child(5) > div:nth-child(4)");
 whitespace.className = "keys whitespace-key";
+
+document.addEventListener('keydown', function (event) {
+  switch (event.key) {
+    case 'Backspace':
+      backspace.classList.add('pressed');
+      break;
+    case 'Tab':
+      tab.classList.add('pressed');
+      break;
+    case 'Delete':
+      del.classList.add('pressed');
+      break;
+    case 'CapsLock':
+      capsLock.classList.toggle('pressed');
+      break;
+    case 'Enter':
+      enter.classList.add('pressed');
+      break;
+    case 'Meta':
+      meta.classList.add('pressed');
+      break;
+    case 'ArrowLeft':
+      arrowLeft.classList.add('pressed');
+      break;
+    case 'ArrowUp':
+      arrowUp.classList.add('pressed');
+      break;
+    case 'ArrowDown':
+      arrowDown.classList.add('pressed');
+      break;
+    case 'ArrowRight':
+      arrowRight.classList.add('pressed');
+      break;
+    case ' ':
+      whitespace.classList.add('pressed');
+      break;
+  }
+  switch (event.code) {
+    case 'ShiftLeft':
+      shiftLeft.classList.add('pressed');
+      break;
+    case 'ShiftRight':
+      shiftRight.classList.add('pressed');
+      break;
+    case 'ControlLeft':
+      controlLeft.classList.add('pressed');
+      break;
+    case 'ControlRight':
+      controlRight.classList.add('pressed');
+      break;
+    case 'AltLeft':
+      altLeft.classList.add('pressed');
+      break;
+    case 'AltRight':
+      altRight.classList.add('pressed');
+      break;
+  }
+});
+
+document.addEventListener('keyup', function (event) {
+  switch (event.key) {
+    case 'Backspace':
+      backspace.classList.remove('pressed');
+      break;
+    case 'Tab':
+      tab.classList.remove('pressed');
+      break;
+    case 'Delete':
+      del.classList.remove('pressed');
+      break;
+    case 'Enter':
+      enter.classList.remove('pressed');
+      break;
+    case 'Meta':
+      meta.classList.remove('pressed');
+      break;
+    case 'ArrowLeft':
+      arrowLeft.classList.remove('pressed');
+      break;
+    case 'ArrowUp':
+      arrowUp.classList.remove('pressed');
+      break;
+    case 'ArrowDown':
+      arrowDown.classList.remove('pressed');
+      break;
+    case 'ArrowRight':
+      arrowRight.classList.remove('pressed');
+      break;
+    case ' ':
+      whitespace.classList.remove('pressed');
+      break;
+  }
+  switch (event.code) {
+    case 'ShiftLeft':
+      shiftLeft.classList.remove('pressed');
+      break;
+    case 'ShiftRight':
+      shiftRight.classList.remove('pressed');
+      break;
+    case 'ControlLeft':
+      controlLeft.classList.remove('pressed');
+      break;
+    case 'ControlRight':
+      controlRight.classList.remove('pressed');
+      break;
+    case 'AltLeft':
+      altLeft.classList.remove('pressed');
+      break;
+    case 'AltRight':
+      altRight.classList.remove('pressed');
+      break;
+  }
+});
